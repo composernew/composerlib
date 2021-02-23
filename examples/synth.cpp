1fabb47e05005a40c84aa79b0d798824d4c504c1
carlasanches <carlasanches8@gmail.com>
2021-02-23 16:00:11 -0300

    Update audio example
    This example was taken from Cycfi Q project. At this stage the program is able to:
    	* Recognize the MIDI devices and audio outputs plugged on PC.
    	* Reproduce the midi signals from the device using a sine synthesizer.

--- examples/synth.cpp
+++ examples/synth.cpp
@@ -1,23 +1,39 @@
-//
-// Created by Carla on 03/02/2021.
-//
+/*=============================================================================
+   Copyright (c) 2014-2020 Joel de Guzman. All rights reserved.
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
 
 #include <array>
 #include <audio/synthesizer.h>
+#include <audio/midi_processor.h>
+#include <audio/devices.h>
 
 int main(){
 
-    constexpr int n = 5;
+    q::midi_input_stream::set_default_device(get_midi_device());
+    auto audio_device_id = get_audio_device();
 
-    std::array<q::frequency,n> notes = {440_Hz, 330_Hz, 550_Hz, 100_Hz, 440_Hz};
+    auto env_cfg = q::envelope::config
+        {
+            100_ms      // attack rate
+            , 1_s         // decay rate
+            , -12_dB      // sustain level
+            , 5_s         // sustain rate
+            , 1_s         // release rate
+        };
 
-    for(size_t i = 0; i < notes.size(); i++){
-        sin_synth synth{notes[i]};
+    sin_synth synth{env_cfg, audio_device_id};
+    q::midi_input_stream stream;
+    midi_processor proc{synth};
 
-        synth.start();
-        q::sleep(3_s);
-        synth.stop();
-    }
+    if (!stream.is_valid())
+        return -1;
+
+    synth.start();
+    while (running)
+        stream.process(proc);
+    synth.stop();
 
     return 0;
 }
