85ef139a97b125ed48fc858ca89d647c49dfff1b
carlasanches <carlasanches8@gmail.com>
2021-03-15 19:32:20 -0300

    Creating class application

---
+++ standalone/gui/application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Carla on 15/03/2021.
+//
+
+#include "application.h"
---
+++ standalone/gui/application.h
@@ -0,0 +1,12 @@
+//
+// Created by Carla on 15/03/2021.
+//
+
+#ifndef COMPOSER_APPLICATION_H
+#define COMPOSER_APPLICATION_H
+
+class application {
+
+};
+
+#endif // COMPOSER_APPLICATION_H
