0534f74b49765ec592ada1f544465133bc8d34c0
carlasanches <carlasanches8@gmail.com>
2021-03-18 16:07:38 -0300

    Modifying the Application class according to reviews

--- standalone/gui/main.cpp
+++ standalone/gui/main.cpp
@@ -13,11 +13,9 @@ int main(int argc, char* argv[])
     // Main window background color
     auto constexpr bkd_color = rgba(35, 35, 37, 255);
 
-    // Elements application
-    app _app(argc, argv, "Composer", "com.composer.composer");
-
     // Composer application
-    application composer(bkd_color, _app);
+    application composer(bkd_color, argc, argv);
+    composer.run();
 
     return 0;
 }
