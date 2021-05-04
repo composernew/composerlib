f4829b37501a4bae735e3331f803df33bf837225
carlasanches <carlasanches8@gmail.com>
2021-05-04 20:39:56 -0300

    Midi file example
    An example using SMFProcess class

---
+++ examples/midi_file.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Carla on 04/05/2021.
+//
+
+#include <smf/smf_processor.h>
+
+int main() {
+
+    SMFProcessor processor;
+
+    processor.process("../twinkle.mid");
+
+    return 0;
+}
