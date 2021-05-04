87cb88d72f30c6de9c1a027bf06bdbef5c884cbe
carlasanches <carlasanches8@gmail.com>
2021-05-04 19:15:25 -0300

    Create SMFProcessor class
    A class to process Standard Midi Files.

---
+++ source/smf/smf_processor.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Carla on 04/05/2021.
+//
+
+#include "smf_processor.h"
+#include <MidiFile.h>
+
+void SMFProcessor::read(std::string filename) {
+
+    smf::MidiFile midiFile;
+
+    midiFile.read(filename);
+
+    midiFile.doTimeAnalysis();
+    midiFile.linkNotePairs();
+
+    int tracks = midiFile.getTrackCount();
+    std::cout << "TPQ: " << midiFile.getTicksPerQuarterNote() << std::endl;
+
+    if(tracks > 1) std::cout << "TRACKS " << tracks << std::endl;
+
+    for(int track = 0; track < tracks; track++){
+        if(tracks > 1) std::cout << "\nTrack " << track << std::endl;
+        std::cout << "Tick\tSeconds\tDur\tMessage" << std::endl;
+
+        for(int event = 0; event < midiFile[track].size(); event++){
+            std::cout << std::dec << midiFile[track][event].tick;
+            std::cout << '\t' << std::dec << midiFile[track][event].seconds;
+            std::cout << '\t';
+
+            if(midiFile[track][event].isNoteOn())
+                std::cout << midiFile[track][event].getDurationInSeconds();
+            std::cout << '\t' << std::hex;
+
+            for(int i = 0; i < midiFile[track][event].size(); i++){
+                std::cout << (int)midiFile[track][event][i] << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
+SMFProcessor::SMFProcessor() {
+
+}
---
+++ source/smf/smf_processor.h
@@ -0,0 +1,20 @@
+//
+// Created by Carla on 04/05/2021.
+//
+
+#ifndef COMPOSER_MIDI_PROCESSOR_H
+#define COMPOSER_MIDI_PROCESSOR_H
+
+#include <iostream>
+
+class SMFProcessor {
+
+
+
+  public:
+
+    void read(std::string filename);
+    SMFProcessor();
+};
+
+#endif // COMPOSER_MIDI_PROCESSOR_H
