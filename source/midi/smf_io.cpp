20003a087746e804d67d2de760ed0b5ac4745319
carlasanches <carlasanches8@gmail.com>
2021-05-16 19:29:10 -0300

    Turning smf_reader into smf_io
    Now the smf_io lib can write a standard midi file and save in a filepath passed as a parameter.

--- source/midi/smf_io.cpp
+++ source/midi/smf_io.cpp
@@ -2,10 +2,10 @@
 // Created by Carla on 04/05/2021.
 //
 
-#include "smf_reader.h"
+#include "smf_io.h"
 #include <MidiFile.h>
 
-smf::MidiEventList smf_reader::read(const std::string &filename) {
+smf::MidiEventList smf_io::read(const std::string &filename) {
 
     smf::MidiFile midiFile;
 
@@ -21,4 +21,13 @@ smf::MidiEventList smf_reader::read(const std::string &filename) {
     }
 
     return midiFile[0];
+}
+
+void smf_io::write(smf::MidiEventList eventList, std::string const &filename) {
+
+    smf::MidiFile midifile;
+
+    midifile[0] = eventList;
+
+    midifile.write(filename);
 }
--- source/midi/smf_io.h
+++ source/midi/smf_io.h
@@ -2,8 +2,8 @@
 // Created by Carla on 04/05/2021.
 //
 
-#ifndef COMPOSER_SMF_READER_H
-#define COMPOSER_SMF_READER_H
+#ifndef COMPOSER_SMF_IO_H
+#define COMPOSER_SMF_IO_H
 
 #include <iostream>
 #include <vector>
@@ -13,11 +13,12 @@
 
 // The smf_reader class reads a MIDI file and returns a smf::MidiEventList prepared to be used to play MIDI notes.
 
-class smf_reader {
+class smf_io {
 
   public:
 
     static smf::MidiEventList read(const std::string &filename);
+    static void write(smf::MidiEventList eventList, std::string const &filename);
 };
 
-#endif // COMPOSER_SMF_READER_H
+#endif // COMPOSER_SMF_IO_H
