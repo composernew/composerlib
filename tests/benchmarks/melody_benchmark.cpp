8f00851d584cd0663f567c765c37bac612ccd5c7
carlasanches <carlasanches8@gmail.com>
2021-09-29 15:24:17 -0300

    Remove melody benchmark

--- tests/benchmarks/melody_benchmark.cpp
+++ tests/benchmarks/melody_benchmark.cpp
@@ -4,14 +4,7 @@
 #include <algorithm>
 
 void create_melody(benchmark::State &state) {
-    for (auto _ : state) {
-        std::vector<int> v(state.range(0));
-        //composer::melody m;
-        //m.notes(v);
-        benchmark::DoNotOptimize(v);
-    }
-}
 
-BENCHMARK(create_melody)->Range(2,2000);
+}
 
 BENCHMARK_MAIN();
